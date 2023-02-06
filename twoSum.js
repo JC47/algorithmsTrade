c2d14ba21724d7b9e641f5d9257cae4c7d5fb91f
JC47 <javiercalette@gmail.com>
2023-02-06 16:17:39 -0600

    Two sum

---
+++ twoSum.js
@@ -0,0 +1,13 @@
+const twoSum = (nums, target) => {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (map.has(diff)) {
+            return [map.get(diff), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+}
+
+console.log(twoSum([3,3], 6));
